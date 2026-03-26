21f42f25a73c1796a977028ac999a31df8670047
Антон <sablinanton92@gmail.com>
2026-03-26 19:47:51 +0300

    add vacancy model

---
+++ backend/app/models/vacancy.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from sqlalchemy import String, Integer, Text, DateTime
+from sqlalchemy.orm import mapped_column, Mapped
+
+from app.db.database import Base
+
+
+class Resume(Base):
+    __tablename__ = 'vacancies'
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    company: Mapped[str] = mapped_column(String(255), nullable=False)
+    level: Mapped[str] = mapped_column(String(50), nullable=False)
+    description: Mapped[str] = mapped_column(Text, nullable=False)
+    salary_text: Mapped[str | None] = mapped_column(String(255), nullable=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(), nullable=False)
