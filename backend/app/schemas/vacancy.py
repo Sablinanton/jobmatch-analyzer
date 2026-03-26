93981ade000142b1a13bf308493e959e3faa223c
Антон <sablinanton92@gmail.com>
2026-03-26 20:19:36 +0300

    add vacancy schema

---
+++ backend/app/schemas/vacancy.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from pydantic import BaseModel, Field
+
+
+class VacancyCtrate(BaseModel):
+    title: str = Field(min_length=1, max_length=255)
+    company: str = Field(min_length=1, max_length=255)
+    level: str = Field(min_length=1, max_length=50)
+    description: str = Field(min_length=1)
+    salary_text: str | None = Field(default=None, max_length=255)
+
+
+class VacancyResponse(BaseModel):
+    id: int
+    title: str
+    company: str
+    level: str
+    description: str
+    salary_text: str | None
+    created_at: datetime
+
+    model_config = {'from_attributtes': True}
