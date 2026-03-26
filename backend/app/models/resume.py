9f33c69530e841da8aee6cd5c1da64cffce2ffcb
Антон <sablinanton92@gmail.com>
2026-03-26 19:43:48 +0300

    add resume model

---
+++ backend/app/models/resume.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from sqlalchemy import String, Integer, Text, DateTime
+from sqlalchemy.orm import mapped_column, Mapped
+
+from app.db.database import Base
+
+
+class Resume(Base):
+    __tablename__ = 'resumes'
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    candidate_name: Mapped[str] = mapped_column(String(255), nullable=False)
+    desired_position: Mapped[str] = mapped_column(String(255), nullable=False)
+    years_of_experience: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
+    raw_text: Mapped[str] = mapped_column(Text, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(), nullable=False)
